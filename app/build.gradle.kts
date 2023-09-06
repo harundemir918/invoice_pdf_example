7708537bc39bfaf0eb7779a18ce86f99eaa91ce3
Harun Demir <59452532+harundemir918@users.noreply.github.com>
2023-09-06 17:49:21 +0300

    create pdf function added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "org.harundemir.invoicepdfexample"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,7 +40,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/org/harundemir/invoicepdfexample/MainActivity.kt
+++ app/src/main/java/org/harundemir/invoicepdfexample/MainActivity.kt
@@ -1,11 +1,119 @@
 package org.harundemir.invoicepdfexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.graphics.pdf.PdfDocument
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.harundemir.invoicepdfexample.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var permissionCode = 101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.createInvoiceButton.setOnClickListener {
+            if (checkStoragePermission()) {
+                createPDF()
+            } else {
+                requestStoragePermission()
+            }
+        }
+    }
+
+    private fun createPDF() {
+        val pdfDocument = PdfDocument()
+        val pageWidth = 595
+        val pageHeight = 842
+        val pageInfo = PdfDocument.PageInfo.Builder(pageWidth, pageHeight, 1).create()
+        val page = pdfDocument.startPage(pageInfo)
+        val canvas = page.canvas
+
+        val companyNamePaint = Paint()
+
+        companyNamePaint.apply {
+            textAlign = Paint.Align.LEFT
+            typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
+            textSize = 12f
+        }
+        canvas.drawText(
+            getString(R.string.app_name),
+            30f, 60f, companyNamePaint
+        )
+
+        pdfDocument.finishPage(page)
+
+        val directory = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            applicationContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        } else {
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+        }
+        val file = File(
+            directory,
+            "invoice.pdf"
+        )
+        try {
+            pdfDocument.writeTo(FileOutputStream(file))
+            Toast.makeText(this, "PDF file created.", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(this, "Failed to create PDF file.", Toast.LENGTH_SHORT).show()
+        }
+        pdfDocument.close()
+    }
+
+    private fun checkStoragePermission(): Boolean {
+        val writeStoragePermission = ContextCompat.checkSelfPermission(
+            applicationContext,
+            WRITE_EXTERNAL_STORAGE
+        )
+        val readStoragePermission = ContextCompat.checkSelfPermission(
+            applicationContext,
+            READ_EXTERNAL_STORAGE
+        )
+        return writeStoragePermission == PackageManager.PERMISSION_GRANTED && readStoragePermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE),
+            permissionCode
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == permissionCode) {
+            if (grantResults.isNotEmpty()) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1]
+                    == PackageManager.PERMISSION_GRANTED
+                ) {
+                    Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show()
+
+                } else {
+                    Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
     }
 }
