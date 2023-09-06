888dd407c2d7aee521d16657ea3ecf3315a36c76
Harun Demir <59452532+harundemir918@users.noreply.github.com>
2023-09-07 00:39:19 +0300

    company and customer info added

--- app/src/main/java/org/harundemir/invoicepdfexample/MainActivity.kt
+++ app/src/main/java/org/harundemir/invoicepdfexample/MainActivity.kt
@@ -43,14 +43,40 @@ class MainActivity : AppCompatActivity() {
         val page = pdfDocument.startPage(pageInfo)
         val canvas = page.canvas
 
-        val companyNamePaint = Paint()
-        val invoicePaint = Paint()
+        val defaultAlignedLeft = Paint().apply {
+            textAlign = Paint.Align.LEFT
+            typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
+            textSize = 12f
+        }
 
-        companyNamePaint.apply {
+        val defaultAlignedRight = Paint().apply {
+            textAlign = Paint.Align.RIGHT
+            typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
+            textSize = 12f
+        }
+
+        val boldAlignedLeft = Paint().apply {
             textAlign = Paint.Align.LEFT
             typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
             textSize = 12f
         }
+
+        val companyNamePaint = Paint(boldAlignedLeft)
+        val invoicePaint = Paint()
+
+        val companyAddressLine1Paint = Paint(defaultAlignedLeft)
+        val companyAddressLine2Paint = Paint(defaultAlignedLeft)
+        val companyPhonePaint = Paint(defaultAlignedLeft)
+        val companyFaxPaint = Paint(defaultAlignedLeft)
+        val invoiceNoPaint = Paint(defaultAlignedRight)
+        val invoiceDatePaint = Paint(defaultAlignedRight)
+
+        val customerInfoTitlePaint = Paint(boldAlignedLeft)
+        val customerNamePaint = Paint(defaultAlignedLeft)
+        val customerAddressLine1Paint = Paint(defaultAlignedLeft)
+        val customerAddressLine2Paint = Paint(defaultAlignedLeft)
+        val customerPhonePaint = Paint(defaultAlignedLeft)
+
         invoicePaint.apply {
             textAlign = Paint.Align.RIGHT
             typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
@@ -58,16 +84,60 @@ class MainActivity : AppCompatActivity() {
             color = Color.parseColor("#595959")
         }
 
-        canvas.drawText(
-            getString(R.string.app_name),
-            40f, 60f, companyNamePaint
-        )
-        canvas.drawText(
-            getString(R.string.invoice).uppercase(),
-            (pageWidth - 40).toFloat(),
-            60f + (companyNamePaint.textSize / 4),
-            invoicePaint
-        )
+        canvas.apply {
+            drawText(
+                getString(R.string.app_name),
+                40f, 60f, companyNamePaint
+            )
+            drawText(
+                getString(R.string.invoice).uppercase(),
+                (pageWidth - 40).toFloat(),
+                60f + (companyNamePaint.textSize / 4),
+                invoicePaint
+            )
+
+            drawText(
+                "1234 Elm Street, East", 40f, 120f, companyAddressLine1Paint
+            )
+            drawText(
+                "Anytown, USA 12345", 40f, 140f, companyAddressLine2Paint
+            )
+            drawText(
+                getString(R.string.phone_with_value, "555-123-4567"), 40f, 160f, companyPhonePaint
+            )
+            drawText(
+                getString(R.string.fax_with_value, "555-123-4568"), 40f, 180f, companyFaxPaint
+            )
+            drawText(
+                getString(R.string.invoice_no_with_value, "ABCD1234"),
+                (pageWidth - 40).toFloat(),
+                160f,
+                invoiceNoPaint
+            )
+            drawText(
+                getString(R.string.invoice_date_with_value, "01-01-2023"),
+                (pageWidth - 40).toFloat(),
+                180f,
+                invoiceDatePaint
+            )
+
+            drawText(
+                getString(R.string.billed_to).uppercase(),
+                40f, 210f, customerInfoTitlePaint
+            )
+            drawText(
+                "John Doe", 40f, 230f, customerNamePaint
+            )
+            drawText(
+                "12346 Mel Underpass, West", 40f, 250f, customerAddressLine1Paint
+            )
+            drawText(
+                "Anytown, USA 12347", 40f, 270f, customerAddressLine2Paint
+            )
+            drawText(
+                 "555-124-4548", 40f, 290f, customerPhonePaint
+            )
+        }
 
         pdfDocument.finishPage(page)
 
@@ -85,7 +155,8 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, getString(R.string.pdf_file_created), Toast.LENGTH_SHORT).show()
         } catch (e: Exception) {
             e.printStackTrace()
-            Toast.makeText(this, getString(R.string.failed_to_create_pdf_file), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, getString(R.string.failed_to_create_pdf_file), Toast.LENGTH_SHORT)
+                .show()
         }
         pdfDocument.close()
     }
@@ -122,10 +193,12 @@ class MainActivity : AppCompatActivity() {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1]
                     == PackageManager.PERMISSION_GRANTED
                 ) {
-                    Toast.makeText(this, getString(R.string.permission_granted), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.permission_granted), Toast.LENGTH_SHORT)
+                        .show()
 
                 } else {
-                    Toast.makeText(this, getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.permission_denied), Toast.LENGTH_SHORT)
+                        .show()
                     finish()
                 }
             }
